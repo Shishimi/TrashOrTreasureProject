518da5749692f184a2322dadda9d834238845d58
alisianguyen <alisia.nguyen@ufl.edu>
2025-04-08 18:04:21 -0400

    updated registerpage.vue

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import RegisterPage from '@/Register-Page.vue'
+import Home from '@/Home-Page.vue'
+import LeaderboardPage from '@/Leaderboard-Page.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/leaderboard',
+      component: LeaderboardPage,
+    },
+    {
+       path: '/register',
+       component: RegisterPage,
+     },
+  ],
+})
+
+export default router
--- src/Register-Page.vue
+++ src/Register-Page.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="register">
     <h2>Create an Account</h2>
     <form @submit.prevent="register">
@@ -17,8 +17,8 @@
       <button type="submit">Register</button>
     </form>
 
-    <p v-if="error" style="color: red">{{ error }}</p>
-    <p v-if="success" style="color: green">{{ success }}</p>
+    <p v-if="error" style="color:red">{{ error }}</p>
+    <p v-if="success" style="color:green">{{ success }}</p>
   </div>
 </template>
 
@@ -55,7 +55,74 @@ export default {
           this.error = 'Something went wrong.'
         }
       }
-    },
-  },
+    }
+  }
 }
 </script>
+-->
+
+<template>
+  <div class="login-page">
+    <img class="homeBG" src="./assets/home-bg.png" />
+    <img class="logo" src="./assets/logo.png" />
+    <img class="loginWord" src="./assets/login word.png" />
+
+    <div class="loginForm">
+      <img class="usernameEntry" src="./assets/login username.png" />
+      <img class="passwordEntry" src="./assets/login password.png" />
+      <img class="loginGoButton" src="./assets/login go button.png" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.login-page {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.homeBG {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+}
+
+.logo {
+  width: 200px;
+  position: absolute;
+  left: 1rem;
+  top: 1rem;
+  z-index: 1;
+}
+
+.loginWord {
+  position: relative;
+  display: block;
+  margin: 8rem auto 2rem auto;
+  z-index: 2;
+  width: 300px;
+}
+
+.loginForm {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+  position: relative;
+}
+
+.usernameEntry,
+.passwordEntry,
+.loginGoButton {
+  width: 300px;
+  margin-bottom: 1rem;
+}
+</style>
